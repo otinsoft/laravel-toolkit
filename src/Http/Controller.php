b41a6cfa855670eae97d4c61f158fe5f04fb0989
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-06-17 23:35:29 +0300

    Add base controller

---
+++ src/Http/Controller.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Otinsoft\Toolkit\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Otinsoft\Toolkit\JavaScript\ScriptVariables;
+use Illuminate\Routing\Controller as BaseController;
+
+abstract class Controller extends BaseController
+{
+    /**
+     * Get the authenticated user.
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable
+     */
+    protected function user()
+    {
+        return Auth::user();
+    }
+
+    /**
+     * Inject script variables.
+     *
+     * @param  array|string|\Closure $key
+     * @param  mixed $value
+     * @return void
+     */
+    protected function scriptVariables($key, $value = null)
+    {
+        ScriptVariables::add($key, $value);
+    }
+
+    /**
+     * Create the response for when a request fails validation.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $errors
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function buildFailedValidationResponse(Request $request, array $errors)
+    {
+        return new JsonResponse($errors, 422);
+    }
+}
