d88a2c961969eb1bf6a3816c3d1fe6218a9c22dd
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-06-22 21:31:19 +0300

    Remove js

--- resources/assets/js/filters/format-duration.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import formatDate from '../utils/format-duration'
-
-Vue.filter('formatDuration', val => val ? formatDuration(val) : '')
--- resources/assets/js/filters/format-size.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import formatDate from '../utils/format-size'
-
-Vue.filter('formatSize', val => val ? formatSize(val) : '')
